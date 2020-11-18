cd06cfb6ad023cf352f252a9f1e77184f3bbab62
Atharv19 <71366372+Atharv19@users.noreply.github.com>
2020-11-18 23:03:16 +0530

    Add files via upload

---
+++ lab 5/tempco.sh
@@ -0,0 +1,8 @@
+echo "Enter tempearture in Fahrenheit"
+read f
+
+echo "Temperature in Celsius is :"
+
+celsius=$(echo "scale=4;(5/9)*($f - 32)"|bc)
+
+echo $celsius
---
+++ lab 6/case_char.sh
@@ -0,0 +1,25 @@
+
+echo "Enter the alphabet"
+read char
+
+case $char in
+
+	'a') echo "Vowel"
+	;;
+
+	'e') echo "Vowel"
+	;;
+
+	'i') echo "Vowel"
+	;;
+
+	'o') echo "Vowel"
+	;;
+
+	'u') echo "Vowel"
+	;;
+
+	*) echo "Consonant"
+	;;
+
+esac
